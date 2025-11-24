44b89ebbb5cee960655a998dec273a570eb09e1e
na-ka-mya-stellah <nstellah285@gmail.com>
2025-11-24 22:35:21 +0300

    conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,10 +1,12 @@
 
 // CONDITIONALS
-// if
-// else
-// else if
+// (1)if
+// (2)else
+// (3)else if
+// The TERNARY operator
+// The SWITCH 
 
-
+// (1) The IF statement
 // example 1
 
 var nameGiven = 'emmy'
@@ -39,7 +41,7 @@ if (number === 10){
     console.log('your wrong always')
 }
 
-// The TERNARY operator
+// (2)The TERNARY operator
 // condition ? expression1 : expression2;
 
 // It says 🤔 if condition is true log expression1 and if it is false log expression2
@@ -95,27 +97,116 @@ person.password !==2424 ? console.log ('enter correct password') : console.log (
 // example 7
 
     
-const user = {
-    age : 20,
-    nationality: 'american',
-    religion : 'christian',
-    maritalStatus : 'single',
-    gender : 'male'
-}
+// const user = {
+//     age : 20,
+//     nationality: 'american',
+//     religion : 'christian',
+//     maritalStatus : 'single',
+//     gender : 'male'
+// }
 
-const userInput = {
-    age : Number(prompt('enter your age')),
-    nationality: prompt('enter your nationality'),
-    religion : prompt('enter your religion'),
-    maritalStatus : prompt('enter your marital status'),
-    gender : prompt ('enter your gender')
-};
+// const userInput = {
+//     age : Number(prompt('enter your age')),
+//     nationality: prompt('enter your nationality'),
+//     religion : prompt('enter your religion'),
+//     maritalStatus : prompt('enter your marital status'),
+//     gender : prompt ('enter your gender')
+// };
 
-(userInput.age === user.age &&
-     userInput.nationality === user.nationality &&
-      userInput.religion ===user.religion &&
-       userInput.maritalStatus === user.maritalStatus
-    )
-    ?
+// (userInput.age === user.age &&
+//      userInput.nationality === user.nationality &&
+//       userInput.religion ===user.religion &&
+//        userInput.maritalStatus === user.maritalStatus
+//     )
+//     ?
 
-    console.log ('access granted') : console.log ('access denied')
+//     console.log ('access granted') : console.log ('access denied')
+
+    // (3)The SWITCH
+    // Example 1
+
+    // you create a function moveCommand that takes the direction parameter
+    function moveCommand(direction){
+
+        // you create a variable whatHappens and dont assign it anyhting for now since it will be assigned a value after the direction of the player is chosen
+        var whatHappens;
+
+        // The switch statement checks the value of direction
+
+        switch(direction){
+
+            // if the direction is forward, run code under this case
+            case 'forward'
+            :
+            // you set a variable what happens to this message
+                whatHappens = 'you encounter a monster';
+                break;
+
+                // stop checking other cases we found the match. stop here and exit the switch and return whatHappens for this case
+
+                case 'back':
+                    whatHappens = 'you arrived home';
+                    break;
+
+
+                    case 'right':
+                        whatHappens = 'you found a river';
+                        break;
+
+
+                        case 'left':
+                            whatHappens = 'you run into a troll';
+                            break;
+
+
+                            default:
+                                whatHappens = 'please enter a valid direction';
+                            
+
+        }
+        return whatHappens;
+    }
+    console.log(moveCommand('forward'));
+    console.log(moveCommand('back'));
+    console.log(moveCommand('right'));
+    console.log(moveCommand('left'));
+    console.log(moveCommand('damn'));
+
+    // Example 2
+    const continents = [
+        'africa',
+        'asia',
+        'europe',
+        'north america'
+    ]
+   function theContinents(continents){
+    var result;
+    switch(continents){
+
+        case 'africa':
+            result = 'beautiful nature';
+            break;
+
+        case 'asia':
+            result= 'too much racism';
+            break;
+
+        case 'europe':
+            result = 'hooo the worst part of racism';
+            break;
+
+        case 'north america':
+            result = 'that one i dont know';
+            break;
+
+            default: 
+             result = 'we are not sure of your location please'
+
+    }
+
+    return result;
+   }
+   console.log (theContinents('africa'));
+   console.log (theContinents('asia'));
+   console.log (theContinents('europe'));
+   console.log (theContinents('north america'));
