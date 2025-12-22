579251f42d62409b0828ccd67d79e4a32634c739
na-ka-mya-stellah <nstellah285@gmail.com>
2025-12-22 23:14:11 +0300

    functionalprogramming.js

--- functionalprogramming.js
+++ functionalprogramming.js
@@ -8,3 +8,51 @@ function add(a,b){
 console.log(add(2,3))
 
 // Example 2
+function subtract (x,y){
+    return x-y;
+}
+console.log(subtract(5,3))
+
+// example 3
+function multiply (v,w){
+    return v*w;
+}
+console.log(multiply(2,3))
+
+// Functions that are not PURE FUNCTIONS
+// they modify the external or outer state
+
+// example 1
+let counter = 0;
+function increment(){
+    counter++;
+}
+increment();
+increment();
+increment();
+console.log (counter);
+
+// example 2
+let name = 'john'
+function changeName(newName){
+    name = newName;
+
+}
+changeName('jane')
+console.log(name)
+
+// example 3
+const person = {
+    name : 'phillip',
+    age : 20
+}
+
+function updateProperties(newName,newAge){
+    this.name = newName
+    this.age = newAge
+}
+updateProperties.call(person,'santos',24)
+console.log(person)
+
+
+
